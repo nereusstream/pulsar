6432b83272124d5c72a6ba551ca7e17bd774588b
Denovo1998 <liusinan1998@gmail.com>
2026-07-19 13:35:34 +0800

    [fix][broker] Fence generation task schema v2

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/storage/nereus/NereusGenerationProtocolCapability.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/storage/nereus/NereusGenerationProtocolCapability.java
@@ -23,7 +23,7 @@ import java.util.Map;
 /** Defines and protects the independently negotiated F4 generation protocol capability. */
 public final class NereusGenerationProtocolCapability {
     public static final String PROPERTY = "nereus.generation-protocol";
-    public static final String VERSION = "1";
+    public static final String VERSION = "2";
 
     private NereusGenerationProtocolCapability() {
     }
