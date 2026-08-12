23065b9e7f2cc2fa4c6e4e67c55b86b8e0fa6c97
Denovo1998 <liusinan1998@gmail.com>
2026-08-13 00:31:58 +0800

    [build][broker] Lock Nereus P1 capability artifacts

--- pulsar-broker/build.gradle.kts
+++ pulsar-broker/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     api(project(":pulsar-opentelemetry"))
     implementation(libs.nereus.domain.n1)
     implementation(libs.nereus.metadata.spi.n1)
+    implementation(libs.nereus.metadata.oxia.p1)
     implementation(project(":pulsar-client-messagecrypto-bc"))
     api(project(":pulsar-functions:pulsar-functions-worker"))
     implementation(project(":pulsar-docs-tools")) {
