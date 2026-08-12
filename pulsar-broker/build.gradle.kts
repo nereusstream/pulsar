5562fa0d490886e9002b31b9ccfdbc8bbe4e8255
Denovo1998 <liusinan1998@gmail.com>
2026-08-12 23:53:29 +0800

    build(nereus): lock immutable N1 artifacts

--- pulsar-broker/build.gradle.kts
+++ pulsar-broker/build.gradle.kts
@@ -37,6 +37,8 @@ dependencies {
     api(project(":pulsar-transaction:pulsar-transaction-coordinator"))
     api(project(":pulsar-opentelemetry"))
     implementation(libs.nereus.pulsar.adapter)
+    implementation(libs.nereus.domain.n1)
+    implementation(libs.nereus.metadata.spi.n1)
     implementation(project(":pulsar-client-messagecrypto-bc"))
     api(project(":pulsar-functions:pulsar-functions-worker"))
     implementation(project(":pulsar-docs-tools")) {
